6041e6e09b718c3ce8e568e1a464063bac13693b
siddhant15oo <61053704+siddhant15oo@users.noreply.github.com>
2020-08-13 19:54:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var runner= createSprite(200,200,10,10);
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,0);  
+  drawSprites();
+}
